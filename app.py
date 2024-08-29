d7628b3df7802aa928445e6a3fc0cb21d90c0292
Hermann002 <hermannnzeudeu@gmail.com>
2024-08-29 10:06:12 +0100

    sauvegarde de la base de données vectorielle

--- app.py
+++ app.py
@@ -1,45 +1,29 @@
 import ollama
 import streamlit as st
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import WebBaseLoader, PyPDFLoader
 from langchain_chroma import Chroma
 from langchain_community.embeddings import OllamaEmbeddings
-from chromadb import Client
-from chromadb.utils.embedding_functions import ollama_embedding_function
 
 st.title("Chat with Documents")
 st.caption("this app allows you to chat with document create by expert in data engineering")
 
-if 'history' not in st.session_state:
-    st.session_state.history = []
-
-webpage_url = st.text_input("Enter webpage URL", type="default")
-
 embeddings = OllamaEmbeddings(model="llama3")
+vectorStore = Chroma(persist_directory="./vector_store_db", embedding=embeddings)
+retriever = vectorStore.as_retriever()
 
-if webpage_url:
-    loader = WebBaseLoader(webpage_url)
-    docs = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=10)
-    splits = text_splitter.split_documents(docs)
-    vectorStore = Chroma.from_documents(documents=splits, embedding=embeddings)
-    retriever = vectorStore.as_retriever()
-
-    def ollama_llm(question, context):
-        formatted_prompt = f"question: {question}\n\n context: {context}"
-        response = ollama.chat(model="llama3", messages=[{'role': 'user', "content": formatted_prompt}])
-        return response['message']['content']
+def ollama_llm(question, context):
+    formatted_prompt = f"question: {question}\n\n context: {context}"
+    response = ollama.chat(model="llama3", messages=[{'role': 'user', "content": formatted_prompt}])
+    return response['message']['content']
 
 
-    def combine_docs(docs):
-            return "\n\n".join(doc.page_content for doc in docs)
-            
-    def rag_chain(question):
-        retrieved_docs = retriever.invoke(question)
-        formatted_context = combine_docs(retrieved_docs)
-        return ollama_llm(question, formatted_context)
+def combine_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+        
+def rag_chain(question):
+    retrieved_docs = retriever.invoke(question)
+    formatted_context = combine_docs(retrieved_docs)
+    return ollama_llm(question, formatted_context)
 
-    st.success(f"load {webpage_url} succesfully !")
 
 prompt = st.text_input("ask any question about the webpage")
 
---
+++ db.py
@@ -0,0 +1,15 @@
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader, PyPDFLoader
+from langchain_chroma import Chroma
+from langchain_community.embeddings import OllamaEmbeddings
+
+embeddings = OllamaEmbeddings(model="llama3")
+
+webpage_url = input("Enter webpage URL")
+
+loader = WebBaseLoader(webpage_url)
+docs = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=10)
+splits = text_splitter.split_documents(docs)
+vectorStore = Chroma.from_documents(documents=splits, embedding=embeddings, persist_directory="./vector_store_db")
