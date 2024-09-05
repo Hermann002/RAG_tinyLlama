3c6f0b9879b0f83a9879c5f14b8cd731ca3a442a
Hermann002 <hermannnzeudeu@gmail.com>
2024-09-05 15:26:06 +0200

    api error

--- app.py
+++ app.py
@@ -5,43 +5,38 @@ from decouple import config
 from mistralai import Mistral
 
 import streamlit as st
-from langchain_chroma import Chroma
-from langchain_community.embeddings import OllamaEmbeddings
-
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_mistralai.embeddings import MistralAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from mistralai import Mistral
 
 st.title("Chat with Documents")
 st.caption("this app allows you to chat with document create by expert in data engineering")
 
 api_key = config('MISTRAL_API_KEY')
-
-
+os.environ['MISTRAL_API_KEY'] = api_key
+os.environ['HF_TOKEN'] = config("HF_TOKEN")
 
 client = Mistral(api_key=api_key)
 
-embedding_mistral = client.embeddings.create(
-    model="mistral-embed",
-    inputs=["Embed this sentence.", "As well as this one."],
-)
+# embeddings = MistralAIEmbeddings(model="mistral-embed", mistral_api_key=api_key)
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-# embeddings = OllamaEmbeddings(model="llama3")
-embeddings = MistralAIEmbeddings(model="mistral-embed", mistral_api_key=api_key)
-vectorStore = Chroma(persist_directory="./vector_store_db", embedding_function=embedding_mistral)
+vectorStore = FAISS.load_local("faiss.index", embeddings, allow_dangerous_deserialization=True)
+
 retriever = vectorStore.as_retriever()
+model = "mistral-large-latest"
 
 
-
-def ollama_llm(question, context):
+def _llm(question, context):
     formatted_prompt = f"question: {question}\n\n context: {context}"
-    # response = ollama.chat(model="llama3", messages=[{'role': 'user', "content": formatted_prompt}])
-    response = client.chat.complete(model= 'open-mistral-7b',
-    messages = [
-        {
+    response = client.chat.complete(
+        model=model,
+        messages=[{
             "role": "user",
-            "content": formatted_prompt,
-        },
-    ])
-    return response['message']['content']
+            "content" : formatted_prompt
+        }])
+    return response.choices[0].message.content
 
 
 def combine_docs(docs):
@@ -50,10 +45,10 @@ def combine_docs(docs):
 def rag_chain(question):
     retrieved_docs = retriever.invoke(question)
     formatted_context = combine_docs(retrieved_docs)
-    return ollama_llm(question, formatted_context)
+    return _llm(question, formatted_context)
 
 
-prompt = st.text_input("ask any question about the webpage") + "Répond moi en Français"
+prompt = st.text_input("ask any question about the webpage")
 
 if prompt:
     start_time = time.time()
--- db.py
+++ db.py
@@ -3,6 +3,7 @@ from decouple import config
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_mistralai.embeddings import MistralAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from mistralai import Mistral
@@ -12,7 +13,8 @@ api_key = config("MISTRAL_API_KEY")
 os.environ['HF_TOKEN'] = config("HF_TOKEN")
 
 # embeddings = OllamaEmbeddings(model="llama3")
-embeddings = MistralAIEmbeddings(model="mistral-embed", mistral_api_key=api_key)
+# embeddings = MistralAIEmbeddings(model="mistral-embed", mistral_api_key=api_key)
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 client = Mistral(api_key=api_key)
 
@@ -29,7 +31,10 @@ def vector_docs(pdf_path):
         vectorStore.save_local("faiss.index")
         print("Création d'une nouvelle base de données vectorielle")
     except Exception as e:
-        vectorStore = FAISS.from_documents(documents, embeddings)
+        try : 
+            vectorStore = FAISS.from_documents(documents, embeddings)
+        except Exception as e:
+            vectorStore = FAISS.from_documents(documents, embeddings)
         vectorStore.save_local("faiss.index")
         print("Ajout des données à la base de données vectorielle")
     return vectorStore
@@ -37,4 +42,4 @@ def vector_docs(pdf_path):
 while True:
     pdf_path = input("Enter pdf path ")
     
-    print(vector_docs(pdf_path))
+    vector_docs(pdf_path)
